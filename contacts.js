43eda6aeaa8185b008d0a7938121d410302fa229
Angela Ordonez <angelitaooo@me.com>
2016-11-07 17:00:18 -0500

    pull request changes

--- contacts.js
+++ contacts.js
@@ -5,15 +5,13 @@ var contactItem = (name, phone) => {
 var submitContacts = () => {
 	$('form').submit((event) => {
 		event.preventDefault();
-		var name = $('#contact-name').val();
-		var	phone = $('#contact-phone').val();
-		var html = contactItem(name, phone);
+		var name = $('#contact-name');
+		var	phone = $('#contact-phone');
+		var html = contactItem(name.val(), phone.val());
 
 		$('#contact-list').append(html);
-		$('#contact-name').val('');
-		$('#contact-phone').val('');
-
-		return false;
+		name.val('');
+		phone.val('');
 
 	});
 };
